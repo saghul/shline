054d56fa0b4e0b29bd576ec4f7ee04c0a7cc954e
Saúl Ibarra Corretgé <saghul@gmail.com>
2015-03-27 10:35:23 +0100

    cleanup git segment

--- segments/git.py
+++ segments/git.py
@@ -1,46 +1,47 @@
 
 def add_git_segment():
+    import os
     import re
     import subprocess
 
     def get_git_status():
+        branch = ''
         has_pending_commits = True
         has_untracked_files = False
-        origin_position = ""
-        output = subprocess.Popen(['git', 'status', '--ignore-submodules'],
-                env={"LANG": "C", "HOME": os.getenv("HOME")}, stdout=subprocess.PIPE).communicate()[0]
-        for line in output.split('\n'):
-            origin_status = re.findall(
-                r"Your branch is (ahead|behind).*?(\d+) comm", line)
-            diverged_status = re.findall(r"and have (\d+) and (\d+) different commits each", line)
-            if origin_status:
-                origin_position = " %d" % int(origin_status[0][1])
-                if origin_status[0][0] == 'behind':
-                    origin_position += u'\u21E3'
-                if origin_status[0][0] == 'ahead':
-                    origin_position += u'\u21E1'
-            if diverged_status:
-                origin_position = " %d%c %d%c" % (int(diverged_status[0][0]), u'\u21E1', int(diverged_status[0][1]), u'\u21E3')
+        origin_position = ''
+        env = {"LANG": "C", "HOME": os.getenv("HOME")}
+        try:
+            output = subprocess.check_output(['git', 'status', '--ignore-submodules'], env=env)
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            for line in output.split('\n'):
+                branch_status = re.findall(r"On branch (.*)", line)
+                dettached_status = re.findall(r"HEAD detached at (.*)", line)
+                origin_status = re.findall(r"Your branch is (ahead|behind).*?(\d+) comm", line)
+                diverged_status = re.findall(r"and have (\d+) and (\d+) different commits each", line)
+                if branch_status:
+                    branch = branch_status[0]
+                if dettached_status:
+                    branch = '(Detached)'
+                if origin_status:
+                    origin_position = " %d" % int(origin_status[0][1])
+                    if origin_status[0][0] == 'behind':
+                        origin_position += u'\u21E3'
+                    if origin_status[0][0] == 'ahead':
+                        origin_position += u'\u21E1'
+                if diverged_status:
+                    origin_position = " %d%c %d%c" % (int(diverged_status[0][0]), u'\u21E1', int(diverged_status[0][1]), u'\u21E3')
+                if line.find('nothing to commit') >= 0:
+                    has_pending_commits = False
+                if line.find('Untracked files') >= 0:
+                    has_untracked_files = True
+        return branch, has_pending_commits, has_untracked_files, origin_position
 
-            if line.find('nothing to commit') >= 0:
-                has_pending_commits = False
-            if line.find('Untracked files') >= 0:
-                has_untracked_files = True
-        return has_pending_commits, has_untracked_files, origin_position
-
-    # See http://git-blame.blogspot.com/2013/06/checking-current-branch-programatically.html
-    p = subprocess.Popen(['git', 'symbolic-ref', '-q', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-
-    if 'Not a git repo' in err:
+    branch, has_pending_commits, has_untracked_files, origin_position = get_git_status()
+    if not branch:
         return
 
-    if out:
-        branch = out[len('refs/heads/'):].rstrip()
-    else:
-        branch = '(Detached)'
-
-    has_pending_commits, has_untracked_files, origin_position = get_git_status()
     branch += origin_position
     if has_untracked_files:
         branch += ' +'
@@ -53,9 +54,4 @@ def add_git_segment():
 
     shline.append(' %s %s ' % (shline.branch, branch), fg, bg)
 
-try:
-    add_git_segment()
-except OSError:
-    pass
-except subprocess.CalledProcessError:
-    pass
+add_git_segment()
